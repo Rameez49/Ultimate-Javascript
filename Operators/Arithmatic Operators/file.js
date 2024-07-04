ee40bcfca36e0c8d248ca7312699e84047843f7f
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-07-04 21:37:48 +0500

    arithmatic opertaors

---
+++ Operators/Arithmatic Operators/file.js
@@ -0,0 +1,20 @@
+let x=10;
+let y=5;
+
+// Arithmatic Operators
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(x**y);
+
+// Incremental 
+
+// console.log(x++);
+// console.log(++x);
+
+// Decremental
+
+// console.log(--x);
