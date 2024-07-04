0d5c15d77b3d99d4bf9e3b6d0e169772ad32daeb
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-07-04 21:49:29 +0500

    comparison operator

---
+++ Operators/Comparison Operators/file.js
@@ -0,0 +1,16 @@
+//  Comparison Operators
+
+x = 1;
+
+console.log(x>0);    //true
+
+console.log(x>=1);    //true
+
+console.log(x<1);       //false
+
+console.log(x===1);       //true
+
+console.log(x!==1);         //false
+
+
+//  This is comparison operators
