f0d9984b3cca8742aad2351ad16d12b59fd7dedc
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-07-05 21:22:12 +0500

    Equality Operators

---
+++ Operators/Equality Operators/file.js
@@ -0,0 +1,11 @@
+let a = 1;
+
+let b = '1';
+
+console.log(a==b);   //true 
+
+// because == check just values. thats why a==b is true.
+
+console.log(a===b);  //false
+
+// because === check values and data type a is integer data type and b is string. thats why a===b is false.
