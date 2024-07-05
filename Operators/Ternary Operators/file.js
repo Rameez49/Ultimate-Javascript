a68302a8915d26e05c7bd9520cd13da8a0029cf9
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-07-05 21:30:24 +0500

    Ternary Operators

---
+++ Operators/Ternary Operators/file.js
@@ -0,0 +1,11 @@
+//           Ternary Operators
+
+// If a coustomer have more than 110 points 
+// then they ara a 'gold' coustomer, otherwise,
+// they are a 'silver' coustomer.
+
+let points = 130;
+
+let coustomer = points >=100 ? 'Coustomer are Gold' : 'Coustomer are Silver'; 
+
+console.log(coustomer);
