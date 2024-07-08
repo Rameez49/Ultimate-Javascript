418cefdd85999848a17d4d1e7a6b8b1c62ebcf83
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-07-08 10:20:58 +0500

    Logical Operators

---
+++ Operators/6. Logical Operators/file.js
@@ -0,0 +1,23 @@
+//    Logical Operators
+
+//  &&
+//  if both conditions true then statement are true.
+// if one false then statement are false.
+
+console.log(true && true );     // true
+console.log(true && false);    // false
+console.log(false && false);   // false
+
+
+//  ||
+// if one true then statement are true.
+
+console.log(true || true);    //true
+console.log(true || false);    // true
+console.log(false || false);   // false
+
+//  !
+// if true then false and if false then true.
+
+console.log(!true);    // false
+console.log(!false);   // true
