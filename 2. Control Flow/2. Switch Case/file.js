3ba3b2baab50e9146ec0b0da44bf8063851523d0
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-07-08 11:07:39 +0500

    Switch Case

---
+++ 2. Control Flow/2. Switch Case/file.js
@@ -0,0 +1,16 @@
+//     Switch Case
+
+let role="guest";
+
+switch (role) {
+  case "guest":
+    console.log("Guest User");
+    break;
+
+  case "moderator":
+    console.log("Moderator User");
+    break;
+
+  default:
+    console.log("Unknown User");
+}
