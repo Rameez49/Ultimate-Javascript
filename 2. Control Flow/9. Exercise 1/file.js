920ab015c97472e6181a4076f9e11ac03a29168a
Rameez Ul Hassan <ulhassanr59@gmail.com>
2024-07-11 10:29:00 +0500

    Exercise 1

---
+++ 2. Control Flow/9. Exercise 1/file.js
@@ -0,0 +1,15 @@
+//  Takes two number and return maximum number
+
+let a = prompt("Enter 1 Number");
+let b = prompt("Enter 2 Number");
+
+if (a>b){
+    alert(`Value of 1 number is ${a}. And Value of 2 number is ${b}. So ${a} is greater number`)
+}
+else if(a===b){
+    alert(`Value of 1 number is ${a}. And Value of 2 number is ${
+        b}. So both are equal number`)
+}
+else {
+    alert(`Value of 1 number is ${a}. And Value of 2 number is ${b}. So ${b} is greater number`)
+}
